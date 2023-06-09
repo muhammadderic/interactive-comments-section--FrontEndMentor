372587f804d55f9ed50d18fd2bca7afbaf95384b
muhammadderic <muhammad.donny.e@gmail.com>
2023-06-09 22:39:38 +0700

    Update line height in chatbox, and change data field in chatbox to props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import ChatBox from "./components/ChatBox";
+import { data } from "./data";
 import "./styles/app.scss";
+
+console.log(data.comments);
 
 function App() {
   return (
     <div className="app-container">
-      <ChatBox />
+      <ChatBox data={data.comments[0]} />
     </div>
   );
 }
--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,20 +1,20 @@
 import "../styles/chatbox.scss";
 
-export default function ChatBox() {
+export default function ChatBox({ data: { content, createdAt, score, user: { username, image } } }) {
   return (
     <div className="chat-box-container">
       <div className="detail">
         <div className="photo-wrapper">
-          <img src="/images/avatars/image-amyrobson.png" alt="profile" />
+          <img src={image.png} alt="profile" />
         </div>
-        <p className="name">amyrobson</p>
-        <p className="date">1 month ago</p>
+        <p className="name">{username}</p>
+        <p className="date">{createdAt}</p>
       </div>
-      <p className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, saepe!</p>
+      <p className="comment">{content}</p>
       <div className="control">
         <div className="vote-wrapper">
           <img src="/images/icon-plus.svg" alt="vote" className="icon-plus" />
-          <p className="vote-num">0</p>
+          <p className="vote-num">{score}</p>
           <img src="/images/icon-minus.svg" alt="unvote" className="icon-minus" />
         </div>
         <div className="reply-wrapper">
