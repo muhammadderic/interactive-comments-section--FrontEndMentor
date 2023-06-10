12c2cfbce700a8d9c7d88a13df8e3b31da6606da
muhammadderic <muhammad.donny.e@gmail.com>
2023-06-10 17:27:43 +0700

    Update style of child of chat box

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,10 +3,12 @@ import ChatBox from "./ChatBox"
 export default function Chats({ data }) {
   return (
     <div>
-      <ChatBox data={data} child={false} />
-      {data.replies.length > 0 ? data.replies.map(reply => (
-        <ChatBox key={reply.id} data={reply} child={true} />
-      )) : ""}
+      <ChatBox data={data} />
+      <div className="chatbox-child">
+        {data.replies.length > 0 ? data.replies.map(reply => (
+          <ChatBox key={reply.id} data={reply} />
+        )) : ""}
+      </div>
     </div>
   )
 }
