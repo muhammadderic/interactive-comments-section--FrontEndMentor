5c9d55a52d3c01ec3b246117a11b2fb30554387d
muhammadderic <muhammad.donny.e@gmail.com>
2023-06-10 18:47:48 +0700

    Moving all contents in App.js to Home.js

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Chats from "../components/Chats";
+import CommentBox from "../components/CommentBox";
+import { data } from "./data";
+import "../styles/home.scss";
+
+export default function Home() {
+  return (
+    <div className="app-container">
+      {data.comments.map(comment => (
+        <Chats key={comment.id} data={comment} />
+      ))}
+      <CommentBox />
+    </div>
+  )
+}
